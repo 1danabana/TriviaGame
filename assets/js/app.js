e3c7f9f2e0451e50c5d20bf87edcc3921ab04d7e
unknown <1danabana@gmail.com>
2019-07-26 12:24:02 -0700

    the questions now appear after on click event

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,19 @@
 $('#begin').on('click', function(){
     for(var i = 0; i < questions.length; i++){
-        $('two').append('<h2>' + questions[i].q +'</h2');
+        $('#two').append('<h2>' + questions[i].q +'</h2');
+        for(var x = 0; x >questions[i].a.length; x++);{
+            $('#two')
+        }
     }
     console.log('ban')
 })
 var questions =[{
     q:'what',
     a:['a', 'b', 'c'],
-    ca: 'a'
+    correct: 'a'
 }, {
     q:'who',
     a:['a', 'b', 'c'],
-    ca: 'c'
+    correct: 'c'
 },
 ]
